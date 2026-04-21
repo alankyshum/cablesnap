3674e4164dd46025e7e63ac2877b1714432d6e28
Alan Shum <alankyshum@users.noreply.github.com>
2026-04-21 14:16:59 -0700

    release: v0.25.2

--- app.config.ts
+++ app.config.ts
@@ -4,7 +4,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   ...config,
   name: "CableSnap",
   slug: "cablesnap",
-  version: "0.25.1",
+  version: "0.25.2",
   orientation: "default",
   icon: "./assets/icon.png",
   userInterfaceStyle: "automatic",
