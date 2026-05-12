04620f45a98e95a8124a9f22e26b08e6c0efec2c
techlead-bld <techlead@bld.local>
2026-05-12 01:38:58 +0000

    chore: remove unused jest.architecture.config.js

--- jest.architecture.config.js
+++
@@ -1,9 +0,0 @@
-process.env.NODE_ENV = 'test';
-module.exports = {
-  testEnvironment: 'node',
-  testMatch: ['**/__tests__/architecture-*.test.ts'],
-  transform: {
-    '^.+\\.tsx?$': ['ts-jest', { tsconfig: { module: 'commonjs', esModuleInterop: true } }],
-  },
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-};
